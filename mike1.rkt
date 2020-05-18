a0b27d2b40d998d067dcee440cacab3d4b909d19
Mike Sperber <sperber@deinprogramm.de>
2020-05-18 09:45:39 +0200

    Update.

--- mike1.rkt
+++ mike1.rkt
@@ -1,6 +1,13 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
-#reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname mike1) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+#reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname mike1) (read-case-sensitive #f) (teachpacks ((lib "image.rkt" "teachpack" "deinprogramm" "sdp"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image.rkt" "teachpack" "deinprogramm" "sdp")))))
 ; Schulung funktionale Programmierung
 
 (define mike "sperber")
+
+(define x
+  (+ 12
+     (* 5 13)))
+
+
+(define circle1 (circle 50 "solid" "red"))
