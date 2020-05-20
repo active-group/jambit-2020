c239090502e4865ca634909c601a8a0da1a027d8
Mike Sperber <sperber@deinprogramm.de>
2020-05-20 16:38:53 +0200

    Update.

--- haskell-code-mike/Lens.hs
+++ haskell-code-mike/Lens.hs
@@ -1,5 +1,24 @@
 module Lens where
 
-data Animal =
+data Dillo =
     Dillo { dilloAlive :: Bool, dilloWeight :: Integer}
 
+-- dilloAlive :: Dillo -> Bool
+
+updateDilloAlive :: Bool -> Dillo -> Dillo
+updateDilloAlive newAlive (Dillo _ weight) =
+    Dillo newAlive weight
+
+data Lens container attribute =
+    Lens {
+        lensGetter :: container -> attribute,
+        lensUpdater :: attribute -> container -> container
+    }
+
+alive = Lens dilloAlive updateDilloAlive
+
+get :: Lens container attribute -> container -> attribute
+get (Lens getter _) container = getter container
+
+
+--- get alive dillo1
