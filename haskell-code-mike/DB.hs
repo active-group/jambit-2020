26fb5c9e47e7e3be17be667ba4bc87955557ccd3
Mike Sperber <sperber@deinprogramm.de>
2020-05-20 15:18:17 +0200

    Update.

--- haskell-code-mike/DB.hs
+++ haskell-code-mike/DB.hs
@@ -1,2 +1,50 @@
 module DB where
 
+import qualified Data.Map.Strict as Map
+
+-- "DSL für Datenbank-Programmierung"
+
+-- put "Mike" 15
+-- x = get "Mike"
+-- put "Mike" (x + 1)
+-- Ergebnis: x
+
+-- 1. Schritt: Datentyp
+
+{-
+data DBCommand =
+    Put String Integer
+  | Get String
+
+put = Put
+get = Get
+
+type DBProgram = [DBCommand]
+
+p1 = let x = get "Mike
+     in [put "Mike" 15,
+         get "Mike",
+         put "Mike" (x + 1)] -- nicht gebunden
+-}
+
+-- Beschreibung einer Datenbankinteraktion
+
+data DB result =
+     Get String (Integer -> DB result) -- Callback, Continuation
+   | Put String Integer (() -> DB result)
+   | Done result
+
+{-
+data Maybe a =
+    Nothing | Just a 
+-}
+
+p1 :: DB (Maybe Integer)
+p1 = Put "Mike" 15 (\() ->
+     Get "Mike" (\ x ->
+     Put "Mike" (x + 1) (\() ->
+     if x > 15
+     then Done (Just x)
+     else Done Nothing)))
+
+runDB :: DB result -> result
