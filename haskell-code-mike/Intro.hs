e2ca44a606e2135341e7092bb993d019a095b5f5
Mike Sperber <sperber@deinprogramm.de>
2020-05-20 09:22:06 +0200

    Update.

--- haskell-code-mike/Intro.hs
+++ haskell-code-mike/Intro.hs
@@ -3,6 +3,13 @@ module Intro where
 x :: Integer
 x = 5 + 7
 
+-- Ein Aggregatzustand ist eins der folgenden:
+-- - fest
+-- - flüssig
+-- - gasförmig
+
+-- data: *neuer* Typ
 data State =
   Solid | Liquid | Gas
   deriving (Show, Eq) -- generiert Typklassen-Instanzen für Show, Eq
+
